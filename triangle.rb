075901770366cbd6a98e9cde6f21ad623b86c2ac
QuantumWaver <sholl76@gmail.com>
2017-09-15 12:10:09 -0400

    Finish more Koans

--- triangle.rb
+++ triangle.rb
@@ -14,13 +14,17 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  if (a == b) && (b == c) && (a == c)
-    :equilateral
-  elsif (a == b) || (b == c) || (a == c)
-    :isosceles
-  else
-    :scalene
-  end
+  a, b, c = [a, b, c].sort
+  raise TriangleError if a <= 0 || a + b <= c
+  [nil, :equilateral, :isosceles, :scalene][ [a, b, c].uniq.size ]
+
+  # if (a == b) && (b == c) && (a == c)
+  #   :equilateral
+  # elsif (a == b) || (b == c) || (a == c)
+  #   :isosceles
+  # else
+  #   :scalene
+  # end
 end
 
 # Error class used in part 2.  No need to change this code.
